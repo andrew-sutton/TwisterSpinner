15503504e1187610010b9a783da4a9d4d5405083
Andrew Sutton <ajsutton94@gmail.com>
2020-06-25 15:55:32 -0500

    Add project files.

---
+++ TwisterSpinner/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Xml.Schema;
+
+namespace TwisterSpinner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = false;
+            var colors = new string[] { "Green", "Red", "Yellow", "Blue" };
+            var bodyParts = new string[] { "Right Hand", "Left Hand", "Right Foot", "Left Foot" };
+            var r = new Random();
+            var exit = "";
+            Console.WriteLine("Welcome to the Twister Spinner!");
+            Console.WriteLine("Type \"exit\" to exit or hit any key to continue");
+            exit = Console.ReadLine();
+            while (exitCode == false)
+            {
+                Console.WriteLine($"{bodyParts[r.Next(0, 3)]} on {colors[r.Next(0, 3)]}!");
+                exit = Console.ReadLine();
+                if (exit == "exit") { exitCode = true; }
+            } 
+        }
+    }
+}
